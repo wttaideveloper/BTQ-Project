7b03336d945ab8b35e4d53d0f86d195b568f76c8
Balavignarajan <balavignarajan63@gmail.com>
2026-02-14 00:30:57 +0530

    Final production fix: remove vite runtime via define flag

--- server/index.ts
+++ server/index.ts
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
+  if (process.env.NODE_ENV !== "production") {
     const { setupVite } = await import("./vite");
     await setupVite(app, server);
   } else {
